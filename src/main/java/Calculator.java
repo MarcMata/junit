5a0789fb99cbe9f105a0b5b278448ec002a683b3
Marc Mata <marcmata26@gmail.com>
2023-04-27 11:45:31 -0500

    completed warm up exercises

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -4,4 +4,8 @@ public class Calculator {
     public static int add(int num1, int num2){
         return num1 + num2;
     }
+
+    public static double calculateTip(double cost, double tipPerecent){
+        return (cost * (tipPerecent/100));
+    }
 }
--- src/test/java/MyFirstTest.java
+++ src/test/java/MyFirstTest.java
@@ -1,6 +1,8 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
+import java.util.ArrayList;
+import java.util.List;
+import static org.junit.Assert.*;
 
 public class MyFirstTest {
 
@@ -11,8 +13,59 @@ public class MyFirstTest {
         int expectedResult = 14;
 
         assertEquals(expectedResult, Calculator.add(input1,input2));
+    }
+    @Test
+    public void testTipCalculator(){
+        double bill = 22.56;
+        double tipPercent = 15;
+        double tip = 3.38;
 
+        assertEquals(tip, Calculator.calculateTip(bill,tipPercent), 0.01);
+    }
 
+    @Test
+    public void testCodeup(){
+        String str1 = "Codeup";
+        String str2 = "CodeUp";
+
+        assertNotEquals(str1,str2);
+    }
+
+    @Test
+    public void arrayListNotSame(){
+        List<String> languages = new ArrayList<>();
+        List<String> moreLanguages = new ArrayList<>();
+
+        assertNotSame(languages, moreLanguages);
+
+    }
+
+    @Test
+    public void arrayNotSame(){
+        int[] numbers = {1, 2, 3};
+        int[] otherNumbers = new int[3];
+        otherNumbers[0] = 1;
+        otherNumbers[1] = 2;
+        otherNumbers[2] = 3;
+
+        assertArrayEquals(numbers, otherNumbers);
+
+    }
+    @Test
+    public void letsGoTrueFalse(){
+        String language = "PHP";
+
+        assertTrue(language, language.contains("H"));
+        assertFalse(language, language.contains("J"));
+    }
+
+    @Test
+    public void nulls(){
+        String language = null;
+        assertNull(language);
+
+        language = "Java";
+        assertNotNull(language);
     }
 
 }
